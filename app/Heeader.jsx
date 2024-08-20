af65c843b224a07a3cbd106ab395275bb7b5af8c
amirpouya-rzv <amir.rezvani7997@gmail.com>
2024-08-20 06:16:48 -0500

    header changed

--- app/Heeader.jsx
+++ app/Heeader.jsx
@@ -10,7 +10,7 @@ function Header() {
     };
 
     return (
-        <nav className="bg-white border-b-4 border-  dark:bg-gray-900  w-screen   h-20 shadow-lg fixed top-0 z-50">
+        <nav className="bg-white border-b-2 border-teal-800  dark:bg-gray-900  w-screen   h-20 shadow-lg fixed top-0 z-50">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="/download (1).png" className="h-16 w-16 rounded-full shadow-md hover:shadow-xl transition-all duration-300 ease-in-out" alt="Logo" />
